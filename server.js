4df970dbb8087804fb480ed8c4bd620344821470
amin <amin@amin.com>
2024-10-19 21:59:10 +0330

    uipdate

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import http from "http";
-const PORT = 8001;
+const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
-	res.write("Hello World");
-	res.end();
+	// res.setHeader("Content-Type", "text/html");
+	// res.statusCode = 200;
+	res.writeHead(200, { "Content-Type": "application/json" });
+	res.end(JSON.stringify({ message: "hello world" }));
 });
 server.listen(PORT, () => {
 	console.log(`server is running on port ${PORT}`);
