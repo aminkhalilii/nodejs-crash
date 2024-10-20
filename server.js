761f2e39cda3e0366f07a4497b80ca4b1d6a87e7
aminkhalilii <aminkhalili430@yahoo.com>
2024-10-20 10:25:14 +0330

    update

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import http from "http";
 const PORT = 8001;
 const server = http.createServer((req, res) => {
-	res.write("Hello World");
-	res.end();
+	res.setHeader("Content-Type", "text/html");
+	res.end('hieewiri wjfioj weiofj weio ');
 });
 server.listen(PORT, () => {
 	console.log(`server is running on port ${PORT}`);
